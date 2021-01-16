aec5d1579253996ec4edd1366c269f38b6676411
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2021-01-15 20:31:00 -0500

    TMP

--- lib/rbi/cli.rb
+++ lib/rbi/cli.rb
@@ -6,7 +6,7 @@ class RBI
   class CLI < ::Thor
     extend T::Sig
 
-    default_task :rbi
+    default_task :validate
 
     class_option :color, type: :boolean, default: true
     class_option :quiet, type: :boolean, default: false, aliases: :q
@@ -14,56 +14,20 @@ class RBI
 
     map T.unsafe(%w[--version] => :__print_version)
 
-    # desc 'RBI', ''
-    # def rbi(*paths)
-    # paths << '.' if paths.empty?
-    # puts "Listing files to parse..."
-    # files = T.unsafe(Parser).list_files(*paths)
-    # puts "  #{files.size} to parse."
-    # files.each_with_index do |file, index|
-    # puts "  parsing #{file} (#{index}/#{files.size})"
-    # RBI.from_file(file)
-    # end
-    # # rbis.each { |rbi| print(rbi.to_rbi) }
-    # end
-
     desc 'validate', ''
     def validate(*paths)
       paths << '.' if paths.empty?
       files = T.unsafe(Parser).list_files(*paths)
-      files.map { |file| parse(file).validate_duplicates }
+      rbis = parse(files)
+      rbis.each { |rbi| rbi.validate_duplicates }
     end
-
-    # desc 'format', ''
-    # def format(*paths)
-    # paths << '.' if paths.empty?
-    # files = T.unsafe(Parser).list_files(*paths)
-    # files.each do |file|
-    # content_before = File.read(file)
-    # content_after = RBI.from_string(content_before)&.to_rbi
-    # ffile = "#{file}.f"
-    # File.write(ffile, content_after)
-    # system("diff -u #{file} #{ffile}")
-    # FileUtils.rm(ffile)
-    # end
-    # end
-    #
-    # desc 'merge', ''
-    # def merge(path1, path2, *paths)
-    # rbi = RBI.from_file(path1)
-    # [path2, *paths].each do |file|
-    # rbi.index.pretty_print
-    # rbi = rbi.merge(RBI.from_file(file))
-    # end
-    # puts rbi.to_rbi
-    # end
 
     desc 'flatten', ''
     def flatten(path, *paths)
-      files = [path, *paths]
-      files.each do |file|
-        puts RBI.from_file(file)&.flatten&.to_rbi
-      end
+      paths = [path, *paths]
+      files = T.unsafe(Parser).list_files(*paths)
+      rbis = parse(files)
+      rbis.each { |rbi| puts rbi.flatten.to_rbi }
     end
 
     desc '--version', 'Show version'
@@ -84,9 +48,12 @@ class RBI
         Logger.new(level: level, color: color, quiet: quiet)
       end
 
-      def parse(file)
-        logger.debug("Parsing #{file}")
-        T.must(RBI.from_file(file))
+      def parse(files)
+        logger = self.logger
+        files.map do |file|
+          logger.debug("Parsing #{file}")
+          T.must(RBI.from_file(file))
+        end
       end
     end
   end
